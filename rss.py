a0053ac2d86a145fbe7292a4388904d8a75fc345
Alexandre M. Savio <alexsavio@gmail.com>
2017-04-01 22:59:07 +0200

    small code style fix

--- rss.py
+++ rss.py
@@ -546,8 +546,7 @@ class Rss(BotPlugin):
         # get the check date for this new feed
         feeds = sorted(get_feed_dates(feed['entries']))
         if feeds and check_date is None:
-            feed_first_date = feeds[0]
-            check_date = feed_first_date
+            check_date = feeds[0] # first feed's date
         else:
             check_date = arrow.now()
 
